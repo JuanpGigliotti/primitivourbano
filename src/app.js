8ead7a7a2bd02a4dfa2bdb7205cbb5acc050e914
JuanpGigliotti <juanpgigliotti@gmail.com>
2024-09-28 13:03:38 -0300

    cambios ashei

--- src/app.js
+++ src/app.js
@@ -1,21 +1,31 @@
 import express from 'express';
-import path from 'path';
 import __dirname__ from "./utils.js";
+import { handlebars } from 'express-handlebars';
 
 //asi es como se importa las routes
 import cartRouter from './routes/carts.router.js';
 import productsRouter from './routes/products.router.js';
 
-
 const app = express();
+
+app.engine('handlebars', handlebars.engine());
+
+app.set('views', __dirname__ + '/views');
+app.set ('view engine','handlebars'); 
+
+//express json url encoded y las routes de los carts
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use('/api/carts', cartRouter);
+app.use('/api/products', productsRouter);
+
+
+
+
+
 const PORT = 8080;
 
 app.listen(PORT, () => {
-console.log('listening on port ${PORT}');
+console.log(`listening on port ${PORT}`);
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
-
-app.use('/api/carts', cartRouter);
-app.use('/api/products', productsRouter);
